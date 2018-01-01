5402aa068347ffcf6ff19878fa41674258e134f1
ntsk <ridha.h2a26@gmail.com>
2018-01-02 03:10:06 +0900

    Load file from args

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -3,7 +3,14 @@ import java.io.File
 object App {
     @JvmStatic
     fun main(args: Array<String>) {
-        val filePath = "resources/item.csv"
+        val filePath = args.getOrExit(0, { "読み込むファイルパスを指定してください。" })
+
+        val outType = args.getOrExit(1, { "出力するフォーマットを指定してください。" })
+
+        val destination = args.getOrNull(2)
+
+        println("$filePath, $outType, $destination")
+
         val content = FileLoader(File(filePath).also {
             if (!it.exists()) {
                 throw IllegalArgumentException("$filePath にファイルがありません")
@@ -11,14 +18,29 @@ object App {
         }).load()?.let(CSVParser()::parse)
                 ?: throw IllegalArgumentException("ファイルの中身がありません")
 
-        val format = enumValueOf<Format>("JSON")
+        val format = enumValueOf<Format>(outType.toUpperCase())
 
-        val formatter = when(format) {
+        val formatter = when (format) {
             Format.JSON -> JSONFormatter(content)
             Format.MARKDOWN -> MarkdownFormatter(content)
             Format.TEXT -> TextFormatter(content)
+            Format.TABLE -> MarkdownTableFormatter(content)
         }
-        val writer = StdWriter()
+        val writer = if (destination == null) {
+            StdWriter()
+        } else {
+            FileWriter(File(destination).also { it.parentFile?.mkdir() })
+        }
         writer.write(formatter)
+    }
+
+    private inline fun Array<String>.getOrExit(index: Int, lazyMessage: () -> String): String {
+        val item = getOrNull(index)
+        if (item != null) {
+            return item
+        }
+        System.err.println(lazyMessage())
+        System.exit(-1)
+        return ""
     }
 }
