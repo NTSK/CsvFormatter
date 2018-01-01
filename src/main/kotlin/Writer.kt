f4e99ffaee9d95c93f725fdf593587ec39fc2426
ntsk <ridha.h2a26@gmail.com>
2018-01-01 20:01:40 +0900

    Add FileWriter

--- src/main/kotlin/Writer.kt
+++ src/main/kotlin/Writer.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 interface Writer {
     // 出力データは確実に存在する必要があるので、Non-Nullとする
     fun write(formatter: Formatter)
@@ -6,5 +8,18 @@ interface Writer {
 class StdWriter : Writer {
     override fun write(formatter: Formatter) {
         println(formatter.format())
+    }
+}
+
+class FileWriter(private val file: File) : Writer {
+    init {
+        require(file.parentFile?.exists() ?: true)
+    }
+
+    override fun write(formatter: Formatter) {
+        file.createNewFile()
+        file.printWriter().use {
+            it.println(formatter.format())
+        }
     }
 }
